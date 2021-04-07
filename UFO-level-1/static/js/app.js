760c8d3add28b33653ef1235728ea2c88cfcbc7b
Bismaad Minhas <bismaad@gmail.com>
2021-04-07 18:24:06 -0400

    multi-filtering set-up

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,32 +8,36 @@ d3.select(window).on("load", default_table);
 
 // Loop through 'tableData' and use d3 to add data to webpage
 function default_table(){
-tableData.forEach((sightings) => {
-    var record = tbody.append("tr");
-    Object.entries(sightings).forEach(([key, value]) => {
-        var data_value = record.append("td");
-        data_value.text(value);
+    tableData.forEach((sightings) => {
+        var record = tbody.append("tr");
+        Object.entries(sightings).forEach(([key, value]) => {
+            var data_value = record.append("td");
+            data_value.text(value);
+        });
     });
-});
-}
+};
 
+// Create a dictionary to log the id of and value entered in each filter
 var dict_filters = {}
 d3.selectAll(".form-control").on("change", function() {
-dict_filters[this.id] = d3.select(this).property("value");
-})
-
-console.log(dict_filters)
-
-
+    dict_filters[this.id] = d3.select(this).property("value");
+});
 
 // Get a reference to the filter button on the page (id property = 'filter-btn')
-var button = d3.select("#filter-btn");
+var filter_button = d3.select("#filter-btn");
 
 // Get a reference to the form on the webpage
 var form = d3.selectAll("form");
 
+// Create event handlers for the clear button
+var clear_button = d3.select("#clear-btn").on("click", function(){
+    // Remove existing records in tbody
+    tbody.html("");
+    return default_table();
+});
+
 // Create event handlers
-button.on("click", runEnter);
+filter_button.on("click", runEnter);
 form.on("submit", runEnter);
 
 // Create the event handler function for the form
@@ -44,18 +48,12 @@ function runEnter() {
 
     Object.entries(dict_filters).forEach(([key, value]) => {
 
-        var inputValue = value
-        console.log(inputValue)
-        // Get a reference to the input element in the datetime filter
-        // var inputField = d3.select("#datetime");
-
-        // Get the value property of the input element in the datetime filter
-        // var inputValue = inputField_date.property("value");
+        var inputValue = value;
 
         // Check if the input field is blank, if blank then run default_table function
         if (inputValue == 0){
 
-            // Remove any children from the tbody
+            // Remove existing records in tbody
             tbody.html("");
 
             // run the default_table function
@@ -65,24 +63,24 @@ function runEnter() {
         // if input field is not blank then filter the output
         else {
 
-        var filteredData = tableData.filter(results => results.dict_filters[key] === inputValue);
+            var filteredData = tableData.filter(results => results[key] === inputValue);
 
-        // Remove any children from tbody
-        tbody.html("");
+            // Remove existing records in tbody
+            tbody.html("");
 
-        // Loop through 'filteredData' and use d3 to add data to webpage
-        filteredData.forEach((sightings) => {
-            var record = tbody.append("tr");
-            Object.entries(sightings).forEach(([key, value]) => {
-                var data_value = record.append("td");
-                data_value.text(value);
+            // Loop through 'filteredData' and use d3 to add data to webpage
+            filteredData.forEach((sightings) => {
+                var record = tbody.append("tr");
+                Object.entries(sightings).forEach(([key, value]) => {
+                    var data_value = record.append("td");
+                    data_value.text(value);
+                });
             });
-        });
         
-    }
-    })
+        };
+    });
 
-}
+};
 
 
 
