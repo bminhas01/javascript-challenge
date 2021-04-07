be7736e18778ed2c3f226b1473a7093ee3c84f0d
Bismaad Minhas <bismaad@gmail.com>
2021-04-06 20:54:59 -0400

    date filtering output complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,7 +4,10 @@ var tableData = data;
 // Create a variable to reference the table body
 var tbody = d3.select("tbody");
 
+d3.select(window).on("load", default_table);
+
 // Loop through 'tableData' and use d3 to add data to webpage
+function default_table(){
 tableData.forEach((sightings) => {
     var record = tbody.append("tr");
     Object.entries(sightings).forEach(([key, value]) => {
@@ -12,12 +15,13 @@ tableData.forEach((sightings) => {
         data_value.text(value);
     });
 });
+}
 
 // Get a reference to the filter button on the page (id property = 'filter-btn')
 var button = d3.select("#filter-btn");
 
 // Get a reference to the form on the webpage
-var form = d3.select("#form");
+var form = d3.selectAll("form");
 
 // Create event handlers
 button.on("click", runEnter);
@@ -29,17 +33,40 @@ function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Get a reference to the input element (class = 'form-control')
-    var inputField = d3.select("#datetime");
+    // Get a reference to the input element in the datetime filter
+    var inputField_date = d3.select("#datetime");
 
-    // Get the value property of the input element
-    var inputValue = inputField.property("value");
+    // Get the value property of the input element in the datetime filter
+    var inputValue_date = inputField_date.property("value");
 
-    var filteredData = tableData.filter(date_filtered => date_filtered.datetime === inputValue);
+    // Check if the input field is blank, if blank then run default_table function
+    if (inputValue_date.length == 0){
 
-    console.log(filteredData);
+        // Remove any children from the tbody
+        tbody.html("");
 
+        // run the default_table function
+        return default_table();
+    }
+    
+    // if input field is not blank then filter the output
+    else {
 
+    var filteredData_date = tableData.filter(date_filtered => date_filtered.datetime === inputValue_date);
+
+    // Remove any children from tbody
+    tbody.html("");
+
+    // Loop through 'filteredData' and use d3 to add data to webpage
+    filteredData_date.forEach((sightings) => {
+        var record = tbody.append("tr");
+        Object.entries(sightings).forEach(([key, value]) => {
+            var data_value = record.append("td");
+            data_value.text(value);
+        });
+    });
+
+    }
 }
 
 
